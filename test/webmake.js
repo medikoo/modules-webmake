a32e5025f3bef20f55213ade4422008c2e05ba14
Mariusz Nowak <medyk@medikoo.com>
2011-08-19 11:18:40 +0200

    Test generated program in clean ES5 context

--- test/webmake.js
+++ test/webmake.js
@@ -1,9 +1,10 @@
 'use strict';
 
-var fs         = require('fs')
-  , startsWith = require('es5-ext/lib/String/starts-with').call
-  , lock       = require('es5-ext/lib/Function/lock').call
-  , ba2p       = require('deferred/lib/async-to-promise').bind
+var fs              = require('fs')
+  , runInNewContext = require('vm').runInNewContext
+  , startsWith      = require('es5-ext/lib/String/starts-with').call
+  , lock            = require('es5-ext/lib/Function/lock').call
+  , ba2p            = require('deferred/lib/async-to-promise').bind
 
   , writeFile = ba2p(fs.writeFile), readFile = ba2p(fs.readFile)
   , unlink = ba2p(fs.unlink)
@@ -17,35 +18,29 @@ module.exports = {
 		t = ba2p(t);
 		t(input)
 		(function (result) {
-			return writeFile(output, 'module.exports = ' + result)
-			(function () {
-				var program = require(output);
-				a(program.x.name, 'x', "Same path require");
-				a(program.x.getZ().name, 'z', "Deferred call");
-				a(program.x.getZ(), program.x.getZ(),
-					"Requiring same object twice, should return same object");
-				a(program.y.z.name, 'z', "Require within required module");
-				a(program.y.z.y.name, 'y', "Circular dependency");
-				a(program.indexed.name, 'indexed', "Folder index");
-				a(program.outer.name, 'outer', "Require module up tree");
-				a(program.external.other.name, 'external-other',
-					"Require module from other package");
-				a(program.external.main.name, 'external-main',
-					"Require main module from other package");
-				a(program.external.main.module.name, 'module',
-					"Require module within other package");
-				a(program.external.noMain.name, 'no-main',
-					"Require from package that doesn't have main module");
+			var program = runInNewContext(result, {});
+			a(program.x.name, 'x', "Same path require");
+			a(program.x.getZ().name, 'z', "Deferred call");
+			a(program.x.getZ(), program.x.getZ(),
+				"Requiring same object twice, should return same object");
+			a(program.y.z.name, 'z', "Require within required module");
+			a(program.y.z.y.name, 'y', "Circular dependency");
+			a(program.indexed.name, 'indexed', "Folder index");
+			a(program.outer.name, 'outer', "Require module up tree");
+			a(program.external.other.name, 'external-other',
+				"Require module from other package");
+			a(program.external.main.name, 'external-main',
+				"Require main module from other package");
+			a(program.external.main.module.name, 'module',
+				"Require module within other package");
+			a(program.external.noMain.name, 'no-main',
+				"Require from package that doesn't have main module");
 
-				return unlink(output)
-				(function () {
-					return t(input, output)
-					(lock(readFile, output, 'utf8'))
-					(function (content) {
-						a(result, content, "Write to file");
-						return unlink(output);
-					});
-				});
+			return t(input, output)
+			(lock(readFile, output, 'utf8'))
+			(function (content) {
+				a(result, content, "Write to file");
+				return unlink(output);
 			});
 		}).cb(d);
 	},
