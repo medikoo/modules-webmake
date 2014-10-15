452837fba74d85dd3154056f3026c46bf373fa82
Mariusz Nowak <medikoo@Mariuszs-MacBook-Pro.local>
2014-10-15 09:38:17 +0200

    Allow per module code transforms

--- test/lib/parser.js
+++ test/lib/parser.js
@@ -1,8 +1,10 @@
 'use strict';
 
-var resolve = require('path').resolve
+var deferred = require('deferred')
+  , resolve  = require('path').resolve
+  , readFile = require('fs2/read-file')
 
-  , pg = resolve(__dirname, '../__playground');
+  , keys = Object.keys, pg = resolve(__dirname, '../__playground');
 
 module.exports = {
 	"": function (t, a, d) {
@@ -13,6 +15,22 @@ module.exports = {
 			a(path, "__playground/lib/program", "Path");
 			a.deep(Object.keys(parser.modules).sort(),
 				['__playground', 'no-main', 'path', 'test'], "Modules");
+		}).done(d, d);
+	},
+	Transform: function (t, a, d) {
+		var input, parser, map = {};
+		input = resolve(pg, 'lib/program.js');
+		parser = t({ transform: function (filename, code) {
+			map[filename] = code;
+			return "'use strict'; module.exports = 'foo';";
+		} });
+		parser.readInput(input)(function (path) {
+			a(path, "__playground/lib/program", "Path");
+			a.deep(Object.keys(parser.modules).sort(),
+				['__playground'], "Modules");
+			return deferred.map(keys(map), function (path) {
+				return readFile(path)(function (content) { a(map[path], String(content), path); });
+			})(function () { return null; });
 		}).done(d, d);
 	}
 };
