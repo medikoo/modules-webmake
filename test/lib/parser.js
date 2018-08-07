79302d27b8ae0288fa1a48772fbac91f65d3ff2c
Mariusz Nowak <medyk@medikoo.com>
2018-08-07 17:28:56 +0200

    style: lint

--- test/lib/parser.js
+++ test/lib/parser.js
@@ -1,34 +1,42 @@
 "use strict";
 
-let deferred = require("deferred")
-  , resolve  = require("path").resolve
-  , readFile = require("fs2/read-file")
+const deferred    = require("deferred")
+    , { resolve } = require("path")
+    , readFile    = require("fs2/read-file");
 
-  , keys = Object.keys, pg = resolve(__dirname, "../__playground");
+const { keys } = Object, pg = resolve(__dirname, "../__playground");
 
 module.exports = {
 	""(t, a, d) {
-		let input, parser;
-		input = resolve(pg, "lib/program.js");
-		parser = t();
-		parser.readInput(input)(path => {
-			a(path, "__playground/lib/program", "Path");
-			a.deep(Object.keys(parser.modules).sort(),
-				["__playground", "no-main", "path", "test"], "Modules");
-		}).done(d, d);
+		const input = resolve(pg, "lib/program.js");
+		const parser = t();
+		parser
+			.readInput(input)(path => {
+				a(path, "__playground/lib/program", "Path");
+				a.deep(
+					Object.keys(parser.modules).sort(), ["__playground", "no-main", "path", "test"],
+					"Modules"
+				);
+			})
+			.done(d, d);
 	},
 	Transform(t, a, d) {
-		let input, parser, map = {};
-		input = resolve(pg, "lib/program.js");
-		parser = t({ transform(filename, code) {
-			map[filename] = code;
-			return "'use strict'; module.exports = 'foo';";
-		} });
-		parser.readInput(input)(path => {
-			a(path, "__playground/lib/program", "Path");
-			a.deep(Object.keys(parser.modules).sort(),
-				["__playground"], "Modules");
-			return deferred.map(keys(map), path => readFile(path)(content => { a(map[path], String(content), path); }))(() => null);
-		}).done(d, d);
+		const map = {};
+		const input = resolve(pg, "lib/program.js");
+		const parser = t({
+			transform(filename, code) {
+				map[filename] = code;
+				return "'use strict'; module.exports = 'foo';";
+			}
+		});
+		parser
+			.readInput(input)(path => {
+				a(path, "__playground/lib/program", "Path");
+				a.deep(Object.keys(parser.modules).sort(), ["__playground"], "Modules");
+				return deferred.map(keys(map), filePath =>
+					readFile(filePath)(content => { a(map[filePath], String(content), filePath); })
+				)(() => null);
+			})
+			.done(d, d);
 	}
 };
